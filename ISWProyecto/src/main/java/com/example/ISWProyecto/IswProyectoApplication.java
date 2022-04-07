c98326424ed61c88df8a6f04c01c0d454a65f142
JoseJuanDominguez <68967470+JoseJuanDominguez@users.noreply.github.com>
2022-04-06 23:24:56 -0500

    Add files via upload

---
+++ ISWProyecto/src/main/java/com/example/ISWProyecto/IswProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISWProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IswProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IswProyectoApplication.class, args);
+	}
+
+}
---
+++ ISWProyecto/src/test/java/com/example/ISWProyecto/IswProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISWProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IswProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
