b56cc136245952810cce1268040dc2d6b10ae03f
JoseJuanDominguez <68967470+JoseJuanDominguez@users.noreply.github.com>
2022-04-10 16:58:55 -0500

    Dependencias y BD
    Agregué los paquetes y la conexión a la BD, es importante que cambien la contraseña por el de su usuario root,

--- ISWProyecto/src/main/java/com/example/ISWProyecto/IswProyectoApplication.java
+++ ISWProyecto/src/main/java/com/example/ISWProyecto/IswProyectoApplication.java
@@ -3,7 +3,7 @@ package com.example.ISWProyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class IswProyectoApplication {
 
 	public static void main(String[] args) {
