a2c254c223be279c7cbd661956b581875d43b45c
JoseJuanDominguez <68967470+JoseJuanDominguez@users.noreply.github.com>
2022-04-11 17:00:29 -0500

    Direcciones

---
+++ ISWProyecto/src/main/java/com/example/ISWProyecto/dto/DireccionesDto.java
@@ -0,0 +1,182 @@
+package com.example.ISWProyecto.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.example.ISWProyecto.model.Direcciones;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DireccionesDto {
+	
+	@JsonProperty("iddireccion")
+	private long iddireccion; 
+	
+	@JsonProperty("calle")
+	private String calle; 
+	
+	@JsonProperty("numext")
+	private String numext; 
+	
+	@JsonProperty("numint")
+	private String numint;
+	
+	@JsonProperty("cp")
+	private Integer cp; 
+	
+	@JsonProperty("telcasa")
+	private long telcasa; 
+	
+	@JsonProperty("telmovil")
+	private long telmovil; 
+	
+	@JsonProperty("teltrabajo")
+	private long teltrabajo; 
+	
+	@JsonProperty("ext")
+	private Integer ext; 
+	
+	@JsonProperty("estado")
+	private String estado; 
+	
+	@JsonProperty("alcaldia")
+	private String alcaldia; 
+	
+	@JsonProperty("correo")
+	private String correo;
+	
+	
+	
+	public static DireccionesDto getInstance(Direcciones direcciones) {
+		
+		DireccionesDto direccionesDto = new DireccionesDto(); 
+		if(direcciones!=null) {
+			direccionesDto.setIddireccion(direcciones.getIddireccion());
+			direccionesDto.setAlcaldia(direcciones.getAlcaldia());
+			direccionesDto.setCalle(direcciones.getCalle());
+			direccionesDto.setCorreo(direcciones.getCorreo());
+			direccionesDto.setCp(direcciones.getCp());
+			direccionesDto.setEstado(direcciones.getEstado());
+			direccionesDto.setExt(direcciones.getExt());
+			direccionesDto.setNumext(direcciones.getNumext());
+			direccionesDto.setNumint(direcciones.getNumint());
+			direccionesDto.setTelcasa(direcciones.getTelcasa());
+			direccionesDto.setTelmovil(direcciones.getTelmovil());
+			direccionesDto.setTeltrabajo(direcciones.getTeltrabajo());
+			
+		}
+		return direccionesDto;
+	}
+	
+	
+	public static List<DireccionesDto> getInstance(List<Direcciones> direcciones) {
+		List<DireccionesDto> direccionesDto = new ArrayList<DireccionesDto>();
+		for( Direcciones dir: direcciones) {
+			direccionesDto.add(DireccionesDto.getInstance(dir));
+		}
+		return direccionesDto;
+		
+	}
+	
+	
+	
+	
+
+	public long getIddireccion() {
+		return iddireccion;
+	}
+
+	public void setIddireccion(long l) {
+		this.iddireccion = l;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getNumext() {
+		return numext;
+	}
+
+	public void setNumext(String numext) {
+		this.numext = numext;
+	}
+
+	public String getNumint() {
+		return numint;
+	}
+
+	public void setNumint(String numint) {
+		this.numint = numint;
+	}
+
+	public Integer getCp() {
+		return cp;
+	}
+
+	public void setCp(Integer cp) {
+		this.cp = cp;
+	}
+
+	public long getTelcasa() {
+		return telcasa;
+	}
+
+	public void setTelcasa(long telcasa) {
+		this.telcasa = telcasa;
+	}
+
+	public long getTelmovil() {
+		return telmovil;
+	}
+
+	public void setTelmovil(long telmovil) {
+		this.telmovil = telmovil;
+	}
+
+	public long getTeltrabajo() {
+		return teltrabajo;
+	}
+
+	public void setTeltrabajo(long teltrabajo) {
+		this.teltrabajo = teltrabajo;
+	}
+
+	public Integer getExt() {
+		return ext;
+	}
+
+	public void setExt(Integer ext) {
+		this.ext = ext;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getAlcaldia() {
+		return alcaldia;
+	}
+
+	public void setAlcaldia(String alcaldia) {
+		this.alcaldia = alcaldia;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	} 
+	
+
+}
--- Proyecto-ISW/src/app/app.module.ts
+++ Proyecto-ISW/src/app/app.module.ts
@@ -4,20 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 0361301b (Revert "Merge branch 'main' into main")
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+
     HeaderComponent,
     FooterComponent
+<<<<<<< HEAD
 =======
 
 @NgModule({
   declarations: [
     AppComponent
 >>>>>>> parent of e56a91fd (Servicio rest direcciones)
+=======
+
+    LoginComponent
+
+>>>>>>> parent of 0361301b (Revert "Merge branch 'main' into main")
   ],
   imports: [
     BrowserModule,
